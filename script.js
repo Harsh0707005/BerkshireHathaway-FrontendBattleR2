3bae3016baffd4072c9e29902e65cfb57ce2f790
Harsh0707005 <harshmaster07705@gmail.com>
2024-05-19 02:27:23 +0530

    under dev

--- script.js
+++ script.js
@@ -19,6 +19,25 @@ fetch("json/content.json")
 	C255,161.018,253.42,157.202,250.606,154.389z"/>
 </svg>
             </div>`
+
+            if (data.releases[release].redirect === "/meetingInfo" || data.releases[release].redirect === "/presentations") {
+                item = `<div class="releaseItem">
+                <div class="releaseItemHeadline">
+                    <span>${data["releases"][release].headline}</span>
+                    <span>${data["releases"][release].date}</span>
+                </div>
+                <div>
+                <span style="background-color: blue; color: white;border-radius: 10px; padding: 5px;">Under Development</span>
+                <svg fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+	c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+	C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+	C255,161.018,253.42,157.202,250.606,154.389z"/>
+</svg>
+</div>
+            </div>`
+            }
             releases.insertAdjacentHTML("beforeend", item)
 
             const releaseItem = releases.lastElementChild;
@@ -47,6 +66,26 @@ fetch("json/content.json")
 	C255,161.018,253.42,157.202,250.606,154.389z"/>
 </svg>
             </div>`
+
+            if (data.specials[special].redirect === "/lettersPPF" || data.specials[special].redirect === "/lettersBS" || data.specials[special].redirect === "/lettersWS" || data.specials[special].redirect === "/lettersGiftBS") {
+                item = `<div class="releaseItem">
+                <div class="releaseItemHeadline">
+                <span>${data["specials"][special].headline}</span>
+                <span>${data["specials"][special].date}</span>
+                </div>
+                <div>
+                <span style="background-color: blue; color: white;border-radius: 10px; padding: 5px;">Under Development</span>
+                <svg fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+	c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+	C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+	C255,161.018,253.42,157.202,250.606,154.389z"/>
+</svg>
+</div>
+            </div>`
+            }
+
             specials.insertAdjacentHTML("beforeend", item)
 
             const specialItem = specials.lastElementChild;
@@ -70,6 +109,27 @@ fetch("json/content.json")
 	C255,161.018,253.42,157.202,250.606,154.389z"/>
 </svg>
             </div>`
+
+            if (data.misc[misc].redirect === "/subsidiaries" || data.misc[misc].redirect === "/governance" || data.misc[misc].redirect === "/sustainability" || data.misc[misc].redirect === "/commonStockInfo" || data.misc[misc].redirect === "/facts") {
+                item = `<div class="releaseItem">
+                <div class="releaseItemHeadline">
+                <span>${data["misc"][misc].headline}</span>
+                    <span>${data["misc"][misc].date}</span>
+                </div>
+                <div>
+                <span style="background-color: blue; color: white;border-radius: 10px; padding: 5px;">Under Development</span>
+                <svg fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+	c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+	C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+	C255,161.018,253.42,157.202,250.606,154.389z"/>
+</svg>
+</div>
+            </div>`
+            }
+
+
             miscs.insertAdjacentHTML("beforeend", item)
 
             const miscItem = miscs.lastElementChild;
