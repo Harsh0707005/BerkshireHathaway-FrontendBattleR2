9cf810812f3f612cc27354fe02c44c914f836898
Harsh0707005 <harshmaster07705@gmail.com>
2024-05-19 00:21:40 +0530

    Commit

---
+++ reports/script.js
@@ -0,0 +1,14 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    // console.log(coll[i])
+  coll[i].addEventListener("click", function() {
+    var content = this.nextElementSibling;
+    if (content.style.display === "flex") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "flex";
+    }
+  });
+}
